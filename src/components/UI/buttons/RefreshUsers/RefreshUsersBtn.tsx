3ce178d0daaecdc41abcfc662f3323d15e09d528
Igor Nikiforov <desola2@inbox.ru>
2024-04-29 12:03:21 +0300

    feat: refresh users btn hover styles added

--- src/components/UI/buttons/RefreshUsers/RefreshUsersBtn.tsx
+++ src/components/UI/buttons/RefreshUsers/RefreshUsersBtn.tsx
@@ -1,9 +1,13 @@
 import cl from './RefreshUsersBtn.module.scss';
 
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement>
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 function RefreshUsersBtn(props: Props) {
-  return <button className={cl.btn} {...props}>Refresh Users</button>;
+  return (
+    <button className={cl.btn} {...props}>
+      <div>Refresh Users</div>
+    </button>
+  );
 }
 
 export default RefreshUsersBtn;
